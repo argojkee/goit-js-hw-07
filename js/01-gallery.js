9dcc581f1d7592a8c3e89e5231d926870fce39b4
argojkee <maksymhaidabura@gmail.com>
2023-07-06 14:28:25 +0300

    homework 7

---
+++ js/01-gallery.js
@@ -0,0 +1,43 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryListEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+
+galleryListEl.insertAdjacentHTML("beforeend", markup);
+
+galleryListEl.addEventListener("click", onPictureClick);
+
+function onPictureClick(e) {
+  event.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  galleryListEl.addEventListener("keydown", (e) => {
+    if (e.code !== "Escape") {
+      return;
+    }
+
+    instance.close();
+  });
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,32 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryListEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"  />
+   </a>
+</li>`;
+  })
+  .join("");
+
+galleryListEl.insertAdjacentHTML("beforeend", markup);
+
+galleryListEl.addEventListener("click", onPictureClick);
+
+function onPictureClick(e) {
+  event.preventDefault();
+
+  if (e.target !== "IMG") {
+    return;
+  }
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
